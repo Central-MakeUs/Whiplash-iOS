9228ed3ac33e9a1607b877e6ee8a9be1503e7452
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-06-24 20:28:23 +0900

    FEAT: 프로젝트 생성

---
+++ Whiplash-iOS/Whiplash_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whiplash_iOSApp.swift
+//  Whiplash-iOS
+//
+//  Created by 남경민 on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Whiplash_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
