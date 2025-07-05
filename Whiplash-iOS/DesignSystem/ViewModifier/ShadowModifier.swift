fe513ee9252d0eff859db683dfd1f1620a1da7a0
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-07-06 01:10:43 +0900

    FIX: Shadow Modifier

--- Whiplash-iOS/DesignSystem/ViewModifier/ShadowModifier.swift
+++ Whiplash-iOS/DesignSystem/ViewModifier/ShadowModifier.swift
@@ -5,4 +5,21 @@
 //  Created by 남경민 on 7/6/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct ShadowModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content.shadow(
+            color: Color.black.opacity(0.4),
+            radius: 8,
+            x: 0,
+            y: 4
+        )
+    }
+}
+
+extension View {
+    func shadow1() -> some View {
+        self.modifier(ShadowModifier())
+    }
+}
