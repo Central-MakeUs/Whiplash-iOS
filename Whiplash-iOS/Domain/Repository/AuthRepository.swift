cd67c5df3371a61d36060b71999ebe0773e02a87
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-07-08 18:39:52 +0900

    feat: auth domain - entity, use case, repository

---
+++ Whiplash-iOS/Domain/Repository/AuthRepository.swift
@@ -0,0 +1,13 @@
+//
+//  AuthRepository.swift
+//  Whiplash-iOS
+//
+//  Created by 남경민 on 7/8/25.
+//
+
+import Foundation
+
+public protocol AuthRepository {
+    var signIn: @Sendable () async throws -> SignInInfo { get }
+    var logout: @Sendable () async throws -> Void { get }
+}
