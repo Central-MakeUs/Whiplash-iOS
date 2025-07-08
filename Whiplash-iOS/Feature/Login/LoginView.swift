fef5a2501c608ee256eda1244b84e39716b6167a
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-07-08 17:01:56 +0900

    ui: Login View

--- Whiplash-iOS/Feature/Login/LoginView.swift
+++ Whiplash-iOS/Feature/Login/LoginView.swift
@@ -5,4 +5,34 @@
 //  Created by 남경민 on 7/8/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack {
+            Color.gray900
+                .ignoresSafeArea()
+            VStack(spacing: 8) {
+                
+                Spacer()
+                
+                LoginButton(type: .apple) {
+                    //
+                }
+                LoginButton(type: .kakao) {
+                    //
+                }
+                LoginButton(type: .google) {
+                    //
+                }
+                
+                Spacer().frame(height: 94)
+            }
+            .padding(.horizontal, AppLayout.horizontalPadding)
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
