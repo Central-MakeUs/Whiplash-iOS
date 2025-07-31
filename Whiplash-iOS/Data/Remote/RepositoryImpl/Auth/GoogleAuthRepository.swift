db98d4e0bd29b2438575dc35069e5a7fac4757b7
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-08-01 05:54:46 +0900

    feat: google 토큰 받아서 서버에 전송

---
+++ Whiplash-iOS/Data/Remote/RepositoryImpl/Auth/GoogleAuthRepository.swift
@@ -0,0 +1,52 @@
+//
+//  GoogleAuthRepository.swift
+//  Whiplash-iOS
+//
+//  Created by 남경민 on 8/1/25.
+//
+
+import Foundation
+import ComposableArchitecture
+import Moya
+import UIKit
+
+public struct GoogleAuthRepositoryImpl: AuthRepository {
+    public var signIn: @Sendable () async throws -> SignInInfo
+    public var logout: @Sendable () async throws -> Void
+    
+    public init(
+        signIn: @escaping @Sendable () async throws -> SignInInfo,
+        logout: @escaping @Sendable () async throws -> Void
+    ) {
+        self.signIn = signIn
+        self.logout = logout
+    }
+}
+
+extension GoogleAuthRepositoryImpl: DependencyKey {
+    public static let liveValue: Self = {
+        let apiClient = APIClient()
+        return Self(
+            signIn: { 
+                let idToken = try await GoogleAuthService.shared.signIn()
+
+                let request = SignInRequestDTO(socialType: "GOOGLE",
+                                               token: idToken,
+                                               deviceId: "1",
+                                               originalNonce: "")
+                
+                let response: Response<SignInResponseDTO> = try await apiClient.request(
+                    Response<SignInResponseDTO>.self,
+                    target: .signIn(request)
+                )
+                if response.isSuccess, let data = response.result {
+                    return data.toDomain
+                } else {
+                    throw NSError(domain: "SignIn", code: 0, userInfo: [NSLocalizedDescriptionKey: response.message])
+                }
+            },
+            logout: {
+            }
+        )
+    }()
+}
