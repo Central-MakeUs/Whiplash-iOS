9fa6a03a38d5f0e9c601f2591feb37c97c4436c6
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-08-01 04:31:31 +0900

    fix: isNewMember 값 추가

--- Whiplash-iOS/Data/Remote/DTO/Response/SignInResponseDTO.swift
+++ Whiplash-iOS/Data/Remote/DTO/Response/SignInResponseDTO.swift
@@ -11,13 +11,16 @@ struct SignInResponseDTO: Respondable {
     let accessToken: String
     let refreshToken: String
     let nickname: String
+    let isNewMember: Bool
     
     init(accessToken: String,
          refreshToken: String,
-         nickname: String) {
+         nickname: String,
+         isNewMember: Bool) {
         self.accessToken = accessToken
         self.refreshToken = refreshToken
         self.nickname = nickname
+        self.isNewMember = isNewMember
     }
 }
 
