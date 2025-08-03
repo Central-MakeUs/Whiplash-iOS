e337a7979f6eb627a1ab351044718021785c9ca9
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-08-03 14:20:10 +0900

    feat: navigation router 추가

---
+++ Whiplash-iOS/App/MainView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Whiplash-iOS
+//
+//  Created by 남경민 on 6/24/25.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+/*
+struct MainView: View {
+    @EnvironmentObject private var router: NavigationRouter
+    let store: StoreOf<AppFeature>
+    
+    init(store: StoreOf<AppFeature>) {
+        self.store = store
+    }
+    
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            
+        }
+    }
+}
+
+*/
