7514d9ed347c29214d502841e54862899f700893
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-08-03 14:10:12 +0900

    feat: 알람 도메인 Entity 추가

---
+++ Whiplash-iOS/Domain/Entity/Alarm.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  Whiplash-iOS
+//
+//  Created by 남경민 on 8/2/25.
+//
+
+import Foundation
+
+struct Alarm {
+    var id: Int
+    var title: String
+    var morningNight: String
+    var time: String
+    var repeatDays: String
+    var address: String
+    var isToggleOn: Bool
+}
+
+extension Alarm {
+    static let sampleData: Alarm = .init(
+        id: 1,
+        title: "눈 떠!",
+        morningNight: "오전",
+        time: "00:00",
+        repeatDays: "월,수,금",
+        address: "서울시 중랑구",
+        isToggleOn: true
+    )
+}
