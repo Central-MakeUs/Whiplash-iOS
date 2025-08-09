51650c534a49a7564e5700eb0d42a3d4674312fe
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2025-08-09 12:56:28 +0900

    feat: 온보딩 UI, Feature 추가

--- Whiplash-iOS/Feature/Onboarding/OnboardingFeature.swift
+++ Whiplash-iOS/Feature/Onboarding/OnboardingFeature.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct OnboardingFeature {
+    @ObservableState
+    struct State: Equatable {
+        var currentPage: Int = 0
+        let totalPages: Int = 4
+    }
+
+    enum Action: BindableAction, Equatable {
+        case binding(BindingAction<State>)
+        case onAppear
+        case pageChanged(Int)
+        case nextTapped
+        case backTapped
+        case skipTapped
+        case startTapped
+        case delegate(Delegate)
+
+        enum Delegate: Equatable {
+            case finished // 온보딩 종료 -> AppFeature로 알림
+        }
+    }
+
+    @Dependency(\.onboardingClient) var onboardingClient
+
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                // 필요 시 analytics 등
+                return .none
+
+            case let .pageChanged(index):
+                state.currentPage = index
+                return .none
+
+            case .nextTapped:
+                if state.currentPage < state.totalPages - 1 {
+                    state.currentPage += 1
+                    return .none
+                } else {
+                    return .send(.startTapped)
+                }
+
+            case .backTapped:
+                if state.currentPage > 0 { state.currentPage -= 1 }
+                return .none
+
+            case .skipTapped:
+                state.currentPage = state.totalPages - 1
+                return .none
+
+            case .startTapped:
+                onboardingClient.setSeen(true)
+                return .send(.delegate(.finished))
+
+            case .binding:
+                return .none
+
+            case .delegate:
+                return .none
+            }
+        }
+    }
+}
